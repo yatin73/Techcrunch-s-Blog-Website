7850b005e4fe10831d01e20ce4d97f286b4528a5
Yatin <yatinrathod612@gmail.com>
2021-08-17 19:45:59 +0530

    techcrunch blog website API

--- src/routes/news.js
+++ src/routes/news.js
@@ -4,7 +4,9 @@ const axios = require('axios')
 
 newsRouter.get('', async(req, res) => {
     try {
-        const newsAPI = await axios.get(`https://velvet-reign.com/wp-json/wp/v2/posts/`)
+        
+        const newsAPI = await axios.get(`https://techcrunch.com/wp-json/wp/v2/posts/`)
+
         res.render('news', { articles : newsAPI.data })
     } catch (err) {
         if(err.response) {
@@ -26,7 +28,7 @@ newsRouter.get('/:id', async(req, res) => {
     let articleID = req.params.id
 
     try {
-        const newsAPI = await axios.get(`https://velvet-reign.com/wp-json/wp/v2/posts/${articleID}`)
+        const newsAPI = await axios.get(`https://techcrunch.com/wp-json/wp/v2/posts/${articleID}`)
         res.render('newsSingle', { article : newsAPI.data })
     } catch (err) {
         if(err.response) {
@@ -48,7 +50,7 @@ newsRouter.get('/:id', async(req, res) => {
 newsRouter.post('', async(req, res) => {
     let search = req.body.search;
     try {
-        const newsAPI = await axios.get(`https://velvet-reign.com/wp-json/wp/v2/posts?search=${search}`)
+        const newsAPI = await axios.get(`https://techcrunch.com/wp-json/wp/v2/posts?search=${search}`)
         res.render('newsSearch', { articles : newsAPI.data })
     } catch (err) {
         if(err.response) {
